d71ac03a73506369b9c235acd14d2e1446358bc8
himani chauhan <himani100700@gmail.com>
2020-09-18 18:44:59 +0530

    changes in server file and package.json

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const http = require('http');
 const api = require('./server/routes/api');
 
-const port = 3000;
+// const port = 3000;
 
 const app = express();
 
@@ -18,6 +18,10 @@ app.use('/api' , api);
 app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, "dist/booking-app/index.html"));
 });
+
+const port = process.env.PORT || '3046';
+app.set('port', port);
+const server = http.createServer(app);
 
 app.listen(port, function(){
     console.log("Server is running on localhost:" + port);
