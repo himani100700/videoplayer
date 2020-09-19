3023c71b7ee6762d454f34e55302d1c22fb2e444
himani chauhan <himani100700@gmail.com>
2020-09-19 18:24:41 +0530

    changes in package.json server.js and angular.json

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(bodyParser.json());
 app.use('/api' , api);
 
 app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, "dist/booking-app/index.html"));
+    res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
-const port = process.env.PORT || '3046';
+const port = process.env.PORT || '4200';
 app.set('port', port);
 const server = http.createServer(app);
 
